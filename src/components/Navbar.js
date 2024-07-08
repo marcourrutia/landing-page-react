410f701c021a69915eae5dcd649669be2474e7c0
marcourrutia <m.urrutia1991@gmail.com>
2024-07-08 18:29:00 -0400

    add Bootstrap and Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+export const Navbar = () => {
+  return (    
+    <ul className="nav bg-dark border-bottom border-body d-flex justify-content-end" data-bs-theme="dark">
+      <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="#">
+          Active
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          Link
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          Link
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" aria-disabled="true">
+          Disabled
+        </a>
+      </li>
+    </ul>
+  );
+};
